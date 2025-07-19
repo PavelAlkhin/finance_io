038280237259603036ff6e7ca08d63ba30ff24d7
PavelAlkhin <pavel.alkhin@octogroup.uz>
2025-07-19 09:25:02 +0300

    init

--- src/main/java/com/example/finance/io/exception/ForbiddenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.finance.io.exception;
-
-public class ForbiddenException extends RuntimeException {
-    public ForbiddenException(String message) {
-        super(message);
-    }
-}
